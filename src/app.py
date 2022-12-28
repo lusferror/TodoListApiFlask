e7b39a9132691cf9f61ec463aa2a1a94a46a1544
lusferror <luisalfredorobles09@gmail.com>
2022-12-28 21:11:44 +0000

    finalizando los ejercicios

--- src/app.py
+++ src/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 todos = [{"label":"my first task", "done":False}]
 
-@app.route('/todo')
-def hello_world():
-    return "<h1>Hello!</h1>"
+# @app.route('/todos')#favor agregar letra "s" al endpoint para probar
+# def hello_world():
+#     return "<h1>Hello!</h1>"
 
 @app.route('/todos',methods=['GET'])
 def get_todos():
@@ -17,19 +17,21 @@ def get_todos():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body= request.json
-    todos.extend(request_body)
+    print("este es la data: ",str(request.data))
+    if (str(request.data))!="b''":
+        request_body= json.loads(request.data)
+        todos.append(request_body)
+    else:
+        request_body=[]
     print("Incoming request with the following body", request_body)
     return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    try:
-        print("This is the position to delete: ",position)
-        del(todos[position])
-        return jsonify(todos)
-    except Exception as e:
-        return jsonify(todos)
+    print("This is the position to delete: ",position)
+    del(todos[position])
+    print(todos)
+    return jsonify(todos)
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=3245, debug =True)
