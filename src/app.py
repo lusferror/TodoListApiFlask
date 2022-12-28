0eef1e0c6ac09a0fa1aef5ef1667e67fb147e57c
lusferror <luisalfredorobles09@gmail.com>
2022-12-28 12:58:28 +0000

    actualizando

--- src/app.py
+++ src/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 
 todos = [{"label":"my first task", "done":False}]
 
+@app.route('/todo')
+def hello_world():
+    return "<h1>Hello!</h1>"
+
 @app.route('/todos',methods=['GET'])
 def get_todos():
     return jsonify(todos)
@@ -14,15 +18,18 @@ def get_todos():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body= request.json
-    todos.append(request_body)
+    todos.extend(request_body)
     print("Incoming request with the following body", request_body)
     return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete: ",position)
-    del(todos[position])
-    return jsonify(todos)
+    try:
+        print("This is the position to delete: ",position)
+        del(todos[position])
+        return jsonify(todos)
+    except Exception as e:
+        return jsonify(todos)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=4000, debug =True)
+    app.run(host='0.0.0.0', port=3245, debug =True)
